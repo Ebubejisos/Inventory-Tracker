8d6f19e3de1ae8042b3585874a37ed7dfb7da136
Ebubejisos <anosikegideon007@gmail.com>
2026-04-15 18:49:20 +0100

    included ssl and Ipv4 permission to database connection string.

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,11 @@ import { Pool } from "pg";
 
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  family: 4, // Use IPv4
 });
 
 export default pool;
